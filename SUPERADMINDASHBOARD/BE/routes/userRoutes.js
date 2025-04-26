026c58c6154eb1a3328c654e119ab2468331bbd6
ishnavi1 <vaishnavichandar2019@gmail.com>
2025-04-26 11:44:27 +0530

    Super Admin Customer Verification Changes

--- SUPERADMINDASHBOARD/BE/routes/userRoutes.js
+++ SUPERADMINDASHBOARD/BE/routes/userRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getAllCustomers } = require('../controllers/userController');
+const { getAllCustomers, acceptVerification, rejectVerification } = require('../controllers/userController');
 
+// Route to get all customers
 router.get('/customers', getAllCustomers);
+
+// Route to accept verification
+router.put('/customers/accept/:userId', acceptVerification);
+
+// Route to reject verification
+router.put('/customers/reject/:userId', rejectVerification);
 
 module.exports = router;
