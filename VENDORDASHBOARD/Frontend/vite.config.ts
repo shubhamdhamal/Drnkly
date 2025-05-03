ac5f3b01c977cf836083490ec89e00e3252e4828
kodeosoftwaretechnology <admin@kodeosoftwaretechnology.com>
2025-05-03 16:11:38 +0530

    Update vite.config.ts

--- VENDORDASHBOARD/Frontend/vite.config.ts
+++ VENDORDASHBOARD/Frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['jspdf'],
+  
     exclude: ['html2canvas', 'core-js', 'canvg'],
   },
   build: {
