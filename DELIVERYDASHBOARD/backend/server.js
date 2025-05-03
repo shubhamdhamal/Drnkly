4f10808d51e258140836efb0a543dc5d7bac51af
kodeosoftwaretechnology <admin@kodeosoftwaretechnology.com>
2025-05-03 09:40:10 +0530

    Update server.js

--- DELIVERYDASHBOARD/backend/server.js
+++ DELIVERYDASHBOARD/backend/server.js
@@ -30,5 +30,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB error:', err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
