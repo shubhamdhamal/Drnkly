e2e20552e84abe51c70730e5dd084f22514a1155
Vaishnavi Chandar <121337041+ishnavi1@users.noreply.github.com>
2025-05-03 13:12:30 +0530

    Update vite.config.ts

--- VENDORDASHBOARD/Frontend/vite.config.ts
+++ VENDORDASHBOARD/Frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['html2canvas', 'core-js', 'jspdf', 'canvg'],
