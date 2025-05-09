7c28e5cb9b16d5225bd71bb901e22c1ef17f4422
kodeosoftwaretechnology <admin@kodeosoftwaretechnology.com>
2025-05-09 11:17:03 +0530

    Update IssueController.js

--- VENDORDASHBOARD/Backend/controllers/IssueController.js
+++ VENDORDASHBOARD/Backend/controllers/IssueController.js
@@ -1,4 +1,4 @@
-const Issue = require('../models/Issue');
+const Issue = require('../models/issue');
 const multer = require('multer');
 const path = require('path');
 const jwt = require('jsonwebtoken');
