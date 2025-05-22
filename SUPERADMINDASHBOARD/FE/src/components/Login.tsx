91e18bffe6e7449a2211bdbe0655cb638764ba6c
git1316 <akshay060103@gmail.com>
2025-05-22 16:34:43 +0530

    log1

--- SUPERADMINDASHBOARD/FE/src/components/Login.tsx
+++ SUPERADMINDASHBOARD/FE/src/components/Login.tsx
@@ -48,7 +48,7 @@ function Login({ onLogin }: LoginProps) {
   }
 
   try {
-    const res = await axios.post('http://localhost5003/api/superadmin/login', {
+    const res = await axios.post('https://admin.peghouse.in/api/superadmin/login', {
       emailOrMobile: email,
       password,
     });
