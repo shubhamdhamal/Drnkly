599a97d85a2a32acdb34aadd2def0313136b5afb
Vaishnavi Chandar <121337041+ishnavi1@users.noreply.github.com>
2025-05-28 01:43:14 +0530

    Update authRoutes.js

--- CUSTOMERDASHBOARD/BE/routes/authRoutes.js
+++ CUSTOMERDASHBOARD/BE/routes/authRoutes.js
@@ -5,6 +5,22 @@ const { signup } = require('../controllers/authController');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/idproofs/' }); // Set up file storage for ID Proof
 
+const { sendOtp, verifyOtp, resetPassword } = require('../controllers/authController');
+const { sendRegistrationOtp,  verifyRegistrationOtp} = require('../controllers/authController');
+
+router.post('/send-registration-otp', sendRegistrationOtp);
+
+// Verify registration OTP (optional, if implemented)
+router.post('/verify-registration-otp', verifyRegistrationOtp);
+
+// Send OTP to email
+router.post('/send-email-otp', sendOtp);
+
+// Verify OTP
+router.post('/verify-email-otp', verifyOtp);
+
+// Reset password
+router.post('/reset-password', resetPassword);
 // User Signup Route
 router.post('/signup', upload.single('idProof'), signup);
 
