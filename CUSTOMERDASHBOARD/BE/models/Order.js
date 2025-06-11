977eb5d48b509810b474a573a44e0a3b46469b44
kodeosoftwaretechnology <admin@kodeosoftwaretechnology.com>
2025-06-11 18:58:32 +0530

    Update Order.js

--- CUSTOMERDASHBOARD/BE/models/Order.js
+++ CUSTOMERDASHBOARD/BE/models/Order.js
@@ -10,7 +10,7 @@ const orderSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: false
+    required: true 
   },
   items: [
     {
