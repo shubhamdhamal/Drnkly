377ba1df570e61433a1ebfef0747ca8f5411559c
Vaishnavi Chandar <121337041+ishnavi1@users.noreply.github.com>
2025-06-18 10:53:10 +0530

    Update Dashboard.tsx

--- CUSTOMERDASHBOARD/FE/src/pages/Dashboard.tsx
+++ CUSTOMERDASHBOARD/FE/src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import oldMonkImage from './pop.jpeg';
 import CartCounter from '../components/CartCounter';
 import { sessionManager } from '../utils/sessionManager';
-import mobileBannerImage from '../public/mobile.jpeg';
+import mobileBannerImage from './mobile.jpeg';
 
 // Simplified banner animations CSS with media queries
 const bannerAnimations = `
