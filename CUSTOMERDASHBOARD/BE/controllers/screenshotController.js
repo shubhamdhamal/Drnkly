463c3f807face13bf5183418a7987d636f027552
kodeosoftwaretechnology <admin@kodeosoftwaretechnology.com>
2025-07-28 09:59:01 +0530

    Update and rename uploadController.js to screenshotController.js

--- CUSTOMERDASHBOARD/BE/controllers/screenshotController.js
+++ CUSTOMERDASHBOARD/BE/controllers/screenshotController.js
@@ -1,14 +1,15 @@
 const path = require('path');
-const fs = require('fs');
 
 exports.uploadScreenshot = (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: 'No file uploaded' });
   }
 
-  const filePath = `/uploads/${req.file.filename}`;
+  // Publicly accessible URL via image.peghouse.in domain
+  const fileUrl = `https://image.peghouse.in/uploads/${req.file.filename}`;
+
   return res.status(200).json({
     message: 'Screenshot uploaded',
-    imageUrl: `https://peghouse.in${filePath}` // Public URL
+    imageUrl: fileUrl
   });
 };
